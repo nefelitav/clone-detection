f418e9593e40d03adf450f0c1deb8e0915bbfe50
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2023-12-15 21:04:05 +0100

    Visualisation Excel

---
+++ clone-visualization/Visualize.py
@@ -0,0 +1,27 @@
+import json
+import pandas as pd
+
+with open('//Users//anand//clone-detection//results//VisualLocation.json', 'r') as file:
+    clonesLocations = json.load(file)
+
+with open('//Users//anand//clone-detection//results//VisualLines.json', 'r') as file:
+    clonesLines = json.load(file)
+
+# Prepare data for DataFrame
+data = []
+for key, values in clonesLocations.items():
+    row = [key] + [clonesLines[key]] + [clonesLines[key] * (len(values) + 1)] + values  # Create a row with the key and its list elements
+    data.append(row)
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Column names
+column_names = ["Clone Class"] + ["No. of lines"] + ["Duplicated Lines in Total"] + [f"Clone {i+1}" for i in range(df.shape[1]-3)]
+df.columns = column_names
+
+# Writing DataFrame to Output Excel
+output = '//Users//anand//clone-detection//results//output.xlsx'
+df.to_excel(output, index=False)
+
+print(f"Data written to {output}")
