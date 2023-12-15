9e0a31c76a25ea68ea72217bf59c46341228d6a4
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2023-12-15 14:47:32 +0100

    Test project

--- TestProject/src/main/java/com/example/App.java
+++ TestProject/src/main/java/com/example/App.java
@@ -1,68 +1,53 @@
 package com.example;
 
 /**
- * Hello world!
- *
- */
+ 
+Hello world!**/
 
 public class App
 {
+
     public void f() {
+        if (true) {
+            int a=1;
+            int b=2;
+            int c=3;
+        }
         int a=1;
         int b=2;
         int c=3;
     }
     public void g() {
+        if (true) {
+            int a=1;
+            int b=2;
+            int c=3;
+        }
         int a=1;
         int b=2;
         int c=3;
     }
-    // public void g()
-    // {
-    //     int a=1;
-    //     int b=2;
-    //     int c=3;
-    // }
-    // public void g()
-    // {
-    //     int y=0;
-    //     int a=1;
-    //     int b=2;
-    //     int c=3;
-    //     int i=4;
-    //     int z=4;
-    // }
-    // public void h()
-    // {
-    //     int y=2;
-    //     int a=1;
-    //     if (y>a)
-    //     {
-    //         int b=2;
-    //         int c=3;
-    //     } else {
-    //     int i=5;
-    //     int z=4;
+
+    // public void h() {
+    //     while(1) {
+    //         System.out.println("hello");
     //     }
     // }
-    // public void i()
-    // {
-    //     int y=2;
-    //     int a=1;
-    //     int b=2;
-    //     int c=3;
-    //     int i=5;
-    //     int z=4;
-    //     int y=0;
-    // }
-    // public void j()
-    // {
-    //     int x=0;
-    //     int a=1;
-    //     int n=5;
-    //     int b=2;
-    //     int c=3;
-    //     int w=4;
-    //     int z=4;
-    // }
 }
+
+// Subtrees Algorithm
+// Clone type 1
+
+// We expect 7 clone pairs:
+// 1) body of f - body of g
+// 2) line 13 - line 17
+// 3) line 14 - line 18
+// 4) line 15 - line 19
+// 5) line 23 - line 27
+// 6) line 24 - line 28
+// 7) line 25 - line 29
+
+//
+
+// 3) - 
+// - are not pairs because they are subclones of the first clone pair
